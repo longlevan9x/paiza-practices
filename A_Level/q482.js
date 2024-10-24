3e9c7d2ab10015eccfad64614c9f4c92af82237a
Kivie <longvan1296@gmail.com>
2024-10-24 10:08:57 +0900

    Update q482.js

--- A_Level/q482.js
+++ A_Level/q482.js
@@ -1,4 +1,4 @@
-const lines = initTest2();
+// const lines = initTest2();
 
 function initTest1() {
     const _lines = [];
@@ -42,210 +42,232 @@ function initTest3() {
     return _lines;
 }
 
-// console.table(lines);
-console.log(hasRoad() ?  "YES" : "NO");
+const lines = [];
+let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('b482sample/testcase-1.txt')
+});
 
-function hasRoad() {
-    const strLine1 = lines[0];
-    const line1s = strLine1.split(" ");
-    const h = getHIndex(line1s);
-    const w = getWIndex(line1s);
-    const spaceLines = [...lines];
-    spaceLines.shift();
+lineReader.on('line', function (line) {
+    lines.push(line);
+});
+
+lineReader.on('close', function () {
+
+    // console.table(lines);
+    console.log(hasRoad() ? "YES" : "NO");
+
+    function hasRoad() {
+        const strLine1 = lines[0];
+        const line1s = strLine1.split(" ");
+        const h = getHIndex(line1s);
+        const w = getWIndex(line1s);
+        const spaceLines = [...lines];
+        spaceLines.shift();
 
 
-    if (w < 1 || h < 1 || w > 100 || h > 100) {
-        return false;
+        if (w < 1 || h < 1 || w > 100 || h > 100) {
+            return false;
+        }
+
+        const sIndex = getSIndex(spaceLines, h, w);
+        // console.log("find sIndex", sIndex);
+
+        const foundIndexes = [];
+
+        const foundDir = findExitRecursive(sIndex, foundIndexes, spaceLines, false, h, w);
+        // console.log("foundDir", foundDir);
+        return foundDir;
     }
 
-    const sIndex = getSIndex(spaceLines, h, w);
-    // console.log("find sIndex", sIndex);
+    /**
+     * 
+     * @param {*} currentIndex 
+     * @param {*} foundIndexes 
+     * @param {*} lines 
+     * @param {*} found 
+     * @returns 
+     */
+    function findExitRecursive(currentIndex, foundIndexes, lines, found, h, w) {
+        const exitDirs = findExitDirections(currentIndex, foundIndexes, lines, h, w);
 
-    const foundIndexes = [];
+        // console.log("currentIndex", currentIndex, "exitDirs", exitDirs);
+        found = exitDirs.found;
 
-    const foundDir = findExitRecursive(sIndex, foundIndexes, spaceLines, false);
-    // console.log("foundDir", foundDir);
-    return foundDir;
-}
+        // dieu kien thoat de quy
+        if (found) {
+            return found;
+        }
 
-/**
- * 
- * @param {*} currentIndex 
- * @param {*} foundIndexes 
- * @param {*} lines 
- * @param {*} found 
- * @returns 
- */
-function findExitRecursive(currentIndex, foundIndexes, lines, found) {
-    const exitDirs = findExitDirections(currentIndex, foundIndexes, lines);
+        for (const element of exitDirs.exitIndexDirs) {
+            const exitDirIndex = element;
+            if (found) {
+                break;
+            }
 
-    // console.log("currentIndex", currentIndex, "exitDirs", exitDirs);
-    found = exitDirs.found;
+            found = findExitRecursive(exitDirIndex, foundIndexes, lines, found, h, w);
+        }
 
-    // dieu kien thoat de quy
-    if(found) {
         return found;
     }
 
+    /**
+     * 
+     * @param {*} lines 
+     * @param {*} h 
+     * @param {*} w 
+     * @returns number[]
+     * Index 0 = h
+     * Index 1 = w
+     */
+    function getSIndex(lines, h, w) {
+        const sIndex = [];
+        let foundS = false;
 
-    for (let i = 0; i < exitDirs.exitIndexDirs.length; i++) {
-        const exitDirIndex = exitDirs.exitIndexDirs[i];
-        found = findExitRecursive(exitDirIndex, foundIndexes, lines, found);
-    }
-
-    return found;
-}
-
-/**
- * 
- * @param {*} lines 
- * @param {*} h 
- * @param {*} w 
- * @returns number[]
- * Index 0 = h
- * Index 1 = w
- */
-function getSIndex(lines, h, w) {
-    const sIndex = [];
-    let foundS = false;
-
-    for (let i = 0; i < h; i++) {
-        if (foundS) {
-            break;
-        }
-
-        const strLine = lines[i];
-        for (let j = 0; j < w; j++) {
+        for (let i = 0; i < h; i++) {
             if (foundS) {
                 break;
             }
 
-            const sChar = strLine[j];
-            if (sChar === "S") {
-                sIndex.push(i, j);
-                foundS = true;
+            const strLine = lines[i];
+            for (let j = 0; j < w; j++) {
+                if (foundS) {
+                    break;
+                }
+
+                const sChar = strLine[j];
+                if (sChar === "S") {
+                    sIndex.push(i, j);
+                    foundS = true;
+                }
             }
         }
+
+        return sIndex;
     }
-
-    return sIndex;
-}
-
-/**
- * 
- * @param {*} currentIndex 
- * @param {*} lines 
- * @param {*} h 
- * @param {*} w 
- * @returns {}
- */
-function findExitDirections(currentIndex, foundIndexes, lines) {
-    const exitIndexDirs = [];
-
-    const hIndex = getHIndex(currentIndex);
-    const wIndex = getWIndex(currentIndex);
 
     /**
-     * right, left, top, bottom
+     * 
+     * @param {*} currentIndex 
+     * @param {*} lines 
+     * @param {*} h 
+     * @param {*} w 
+     * @returns {}
      */
-    const indexFourDirs = [
-        [hIndex, wIndex + 1],
-        [hIndex, wIndex - 1],
-        [hIndex - 1, wIndex],
-        [hIndex + 1, wIndex]
-    ];
+    function findExitDirections(currentIndex, foundIndexes, lines, h, w) {
+        const exitIndexDirs = [];
 
-    let found = false;
+        const hIndex = getHIndex(currentIndex);
+        const wIndex = getWIndex(currentIndex);
 
-    for (let i = 0; i < 4; i++) {
-        const char = findExitDirection(lines, indexFourDirs[i][0], indexFourDirs[i][1]);
-        // console.log('char', i, char);
-        const hasDir = isExitDirection(char);
-        const _isFoundIndex = isFoundIndex(indexFourDirs[i], foundIndexes);
-        // console.log("hasDir", hasDir, indexFourDirs[i]);
-        if (hasDir && !_isFoundIndex) {
-            exitIndexDirs.push(indexFourDirs[i]);
-            foundIndexes.push(indexFourDirs[i]);
+        if (hIndex == 0 || wIndex == 0 || hIndex == h - 1 || wIndex == w - 1) {
+            return { found: true, exitIndexDirs: [] };
         }
 
-        if(char === undefined) {
-            found = true;
+        /**
+         * right, left, top, bottom
+         */
+        const indexFourDirs = [
+            [hIndex, wIndex + 1],
+            [hIndex, wIndex - 1],
+            [hIndex - 1, wIndex],
+            [hIndex + 1, wIndex]
+        ];
+
+        let found = false;
+
+        for (let i = 0; i < 4; i++) {
+            const _hIndex = getHIndex(indexFourDirs[i]);
+            const _wIndex = getWIndex(indexFourDirs[i]);
+
+            const char = findExitDirection(lines, _hIndex, _wIndex);
+            // console.log('char', i, char);
+            const hasDir = isExitDirection(char);
+            const _isFoundIndex = isFoundIndex(indexFourDirs[i], foundIndexes);
+            // console.log("hasDir", hasDir, indexFourDirs[i]);
+            if (hasDir && !_isFoundIndex) {
+                exitIndexDirs.push(indexFourDirs[i]);
+                foundIndexes.push(indexFourDirs[i]);
+            }
+
+            if (char === undefined) {
+                found = true;
+            }
         }
+
+        return { exitIndexDirs: exitIndexDirs, found: found };
     }
 
-    return { exitIndexDirs: exitIndexDirs, found: found };
-}
+    function isFoundIndex(currentIndex, foundIndexes) {
+        let found = false;
 
-function isFoundIndex(currentIndex, foundIndexes) {
-    let found = false;
+        for (const element of foundIndexes) {
+            const foundIndex = element;
 
-    for (let i = 0; i < foundIndexes.length; i++) {
-        const foundIndex = foundIndexes[i];
-
-        if (foundIndex[0] === currentIndex[0] && foundIndex[1] === currentIndex[1]) {
-            found = true;
-            break;
+            if (foundIndex[0] === currentIndex[0] && foundIndex[1] === currentIndex[1]) {
+                found = true;
+                break;
+            }
         }
+
+        return found;
     }
 
-    return found;
-}
+    function findExitDirection(lines, hIndex, wIndex) {
+        const line = lines[hIndex];
+        // console.log("line", hIndex, wIndex)
+        // console.log("line", hIndex, wIndex, line);
+        const char = line[wIndex];
 
-function findExitDirection(lines, hIndex, wIndex) {
-    const line = lines[hIndex];
-    // console.log("line", hIndex, wIndex, line);
-    const char = line[wIndex];
+        return char;
+    }
 
-    return char;
-}
+    function isExitDirection(char) {
+        return char === '.';
+    }
 
-function isExitDirection(char) {
-    return char === '.';
-}
+    // function isEdgeIndex(currentIndex, index) {
+    // }
 
-function isEdgeIndex(currentIndex, index,) {
+    // function findStartDirection(exitDirs, lines, currentIndex) {
+    //     if(exitDirs.length === 1) {
+    //         return exitDirs[0];
+    //     }
 
-}
+    //     for (let i = 0; i < exitDirs.length; i++) {
+    //         const exitDirIndex = exitDirs[i];
+    //         const hIndex = getHIndex(exitDirIndex);
+    //         const wIndex = getWIndex(exitDirIndex);
 
-// function findStartDirection(exitDirs, lines, currentIndex) {
-//     if(exitDirs.length === 1) {
-//         return exitDirs[0];
-//     }
+    //         const hIndexCur = getHIndex(currentIndex);
+    //         const wIndexCur = getWIndex(currentIndex);
 
-//     for (let i = 0; i < exitDirs.length; i++) {
-//         const exitDirIndex = exitDirs[i];
-//         const hIndex = getHIndex(exitDirIndex);
-//         const wIndex = getWIndex(exitDirIndex);
+    //         if (hIndexCur === hIndex) {
+    //             const line = lines[hIndex];
 
-//         const hIndexCur = getHIndex(currentIndex);
-//         const wIndexCur = getWIndex(currentIndex);
-
-//         if (hIndexCur === hIndex) {
-//             const line = lines[hIndex];
-
-//             if (wIndex < wIndexCur) {
-//                 const dotCount = "".
-//             }
-//         }
+    //             if (wIndex < wIndexCur) {
+    //                 const dotCount = "".
+    //             }
+    //         }
 
 
-//     }
-// }
+    //     }
+    // }
 
-/**
- * 
- * @param {*} arrs 
- * @returns number
- */
-function getHIndex(arrs) {
-    return arrs[0];
-}
+    /**
+     * 
+     * @param {*} arrs 
+     * @returns number
+     */
+    function getHIndex(arrs) {
+        return arrs[0];
+    }
 
-/**
- * 
- * @param {*} arrs 
- * @returns number
- */
-function getWIndex(arrs) {
-    return arrs[1];
-}
+    /**
+     * 
+     * @param {*} arrs 
+     * @returns number
+     */
+    function getWIndex(arrs) {
+        return arrs[1];
+    }
+});
